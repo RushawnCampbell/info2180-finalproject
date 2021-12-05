8469daa1463e651a95a13821793ed6bac6a2e829
RushawnCampbell <shoutme@sean@gmail.com>
2021-12-05 11:21:20 -0500

    Missing home.js

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS `bugme` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS `bugme`;
 
 GRANT ALL PRIVILEGES ON bugme.* TO 'admin'@'localhost' IDENTIFIED BY 'password123';
 
@@ -6,32 +6,28 @@ USE `bugme`;
 
 DROP TABLE IF EXISTS `userstable`;
 
-CREATE TABLE IF NOT EXISTS `userstable` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `lastname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `date_joined` datetime NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE userstable (
+ id  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  firstname varchar(255) NOT NULL,
+  lastname varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  email varchar(255),
+  date_joined DATETIME
 );
-
-ALTER TABLE userstable AUTO_INCREMENT=6705;
 
 DROP TABLE IF EXISTS `issuestable`;
 
-CREATE TABLE IF NOT EXISTS `issuestable` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `type` varchar(255) NOT NULL,
-  `priority` varchar(255) NOT NULL,
-  `status` varchar(255) NOT NULL,
-  `assigned_to` int NOT NULL,
-  `created_by` int NOT NULL,
-  `created` datetime NOT NULL,
-  `updated` datetime NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE issuestable (
+ id  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title varchar(255) NOT NULL,
+  description TEXT NOT NULL,
+  type varchar(255) NOT NULL,
+  priority varchar(255),
+  status varchar(255) ,
+  assigned_to  int NOT NULL,
+  created_by int NOT NULL,
+  created DATETIME,
+  updated DATETIME
 );
 
 INSERT INTO `userstable` (`firstname`, `lastname`, `email`, `password`, `date_joined`) VALUES
